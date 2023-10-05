c6b72f3a469cf25a3175ef48d74fb22e66877b85
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-10-05 21:06:13 +0530

    Word Replacement Program

---
+++ wordReplace.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str = "hello ajsal how are you"
+    word_to_replace = input("Enter the word to replace: ")
+    word_to_replacement = input("Enter the word to replacement: ")
+    print(str.replace(word_to_replace,word_to_replacement))
+
+replace_word()
